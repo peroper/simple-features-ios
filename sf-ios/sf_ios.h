20e60b5024e234485e6ace364ca00be7bd4163b8
Jonas Bromö <jonas.bromo@gmail.com>
2021-09-28 15:20:03 +0200

    (Re-?)Added Carthage support

--- sf-ios/sf_ios.h
+++ sf-ios/sf_ios.h
@@ -6,8 +6,44 @@
 //  Copyright (c) 2015 NGA. All rights reserved.
 //
 
-#ifndef sf_ios_sf_ios_h
-#define sf_ios_sf_ios_h
-
-
-#endif
+#import <sf_ios/SFCircularString.h>
+#import <sf_ios/SFCompoundCurve.h>
+#import <sf_ios/SFCurve.h>
+#import <sf_ios/SFCurvePolygon.h>
+#import <sf_ios/SFGeometry.h>
+#import <sf_ios/SFGeometryCollection.h>
+#import <sf_ios/SFGeometryEnvelope.h>
+#import <sf_ios/SFGeometryTypes.h>
+#import <sf_ios/SFLine.h>
+#import <sf_ios/SFLinearRing.h>
+#import <sf_ios/SFLineString.h>
+#import <sf_ios/SFMultiCurve.h>
+#import <sf_ios/SFMultiLineString.h>
+#import <sf_ios/SFMultiPoint.h>
+#import <sf_ios/SFMultiPolygon.h>
+#import <sf_ios/SFMultiSurface.h>
+#import <sf_ios/SFPoint.h>
+#import <sf_ios/SFPolygon.h>
+#import <sf_ios/SFPolyhedralSurface.h>
+#import <sf_ios/SFSurface.h>
+#import <sf_ios/SFTIN.h>
+#import <sf_ios/SFTriangle.h>
+#import <sf_ios/SFExtendedGeometryCollection.h>
+#import <sf_ios/SFByteReader.h>
+#import <sf_ios/SFByteWriter.h>
+#import <sf_ios/SFGeometryEnvelopeBuilder.h>
+#import <sf_ios/SFGeometryPrinter.h>
+#import <sf_ios/SFGeometryUtils.h>
+#import <sf_ios/SFCentroidPoint.h>
+#import <sf_ios/SFCentroidCurve.h>
+#import <sf_ios/SFCentroidSurface.h>
+#import <sf_ios/SFEvent.h>
+#import <sf_ios/SFEventQueue.h>
+#import <sf_ios/SFEventTypes.h>
+#import <sf_ios/SFSegment.h>
+#import <sf_ios/SFShamosHoey.h>
+#import <sf_ios/SFSweepLine.h>
+#import <sf_ios/SFFiniteFilterTypes.h>
+#import <sf_ios/SFGeometryFilter.h>
+#import <sf_ios/SFPointFiniteFilter.h>
+#import <sf_ios/SFTextReader.h>
